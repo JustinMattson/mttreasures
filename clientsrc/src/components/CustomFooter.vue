c37c2744076e8bc28f2d1da2ae89f929a478b964
Justin Mattson <justin.mattson@outlook.com>
2020-10-09 21:48:26 -0600

    added link to cbfree facebook

--- clientsrc/src/components/CustomFooter.vue
+++ clientsrc/src/components/CustomFooter.vue
@@ -4,6 +4,12 @@
       <div class="col-6 text-center">
         Christine Brown
         <br />
+        <a href="https://www.facebook.com/christine.brown.3154" target="_blank">
+          <i
+            class="fa fa-facebook-official text-dark"
+            aria-hidden="true"
+          ></i> </a
+        >&nbsp;
         <a href="mailto:cbfreemt@gmail.com" class="text-dark">
           <i class="fa fa-envelope-o text-dark" aria-hidden="true"></i>
         </a>
@@ -13,14 +19,20 @@
           Justin Mattson
           <br />
           <a href="https://www.facebook.com/justin.mattson.9" target="_blank">
-            <i class="fa fa-facebook-official text-dark" aria-hidden="true"></i>
-          </a>&nbsp;
+            <i
+              class="fa fa-facebook-official text-dark"
+              aria-hidden="true"
+            ></i> </a
+          >&nbsp;
           <a href="https://www.linkedin.com/in/justin-mattson" target="_blank">
-            <i class="fa fa-linkedin-square text-dark" aria-hidden="true"></i>
-          </a>&nbsp;
+            <i
+              class="fa fa-linkedin-square text-dark"
+              aria-hidden="true"
+            ></i> </a
+          >&nbsp;
           <a href="https://justinmattson.github.io/" target="_blank">
-            <i class="fa fa-github text-dark p-1" aria-hidden="true"></i>
-          </a>&nbsp;
+            <i class="fa fa-github text-dark p-1" aria-hidden="true"></i> </a
+          >&nbsp;
           <a href="mailto:justin.mattson@outlook.com" class="text-dark">
             <i class="fa fa-envelope-o text-dark" aria-hidden="true"></i>
           </a>
